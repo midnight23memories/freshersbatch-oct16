50e5e731d9625c09fb4989dad2629fa73cefeb96
Simmi1802 <110179790+Simmi1802@users.noreply.github.com>
2024-02-01 17:19:05 +0530

    Add files via upload

---
+++ SampleProj/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter num1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter num2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 * num2;
+
+            Console.WriteLine("Multiplication result: " + result);
+        }
+    }
+}
