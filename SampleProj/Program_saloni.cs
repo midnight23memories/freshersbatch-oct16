1bfef17b30c1d3a4a961a06f4ce27bb4cbe8d42a
salonideshmukh31 <92880307+salonideshmukh31@users.noreply.github.com>
2024-02-01 17:32:53 +0530

    Add files via upload

---
+++ SampleProj/Program_saloni.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_saloni
+{
+    internal class Program_saloni
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number 1=");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number 1=");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Substraction is=");
+            int sub = Convert.ToInt32(Console.ReadLine());
+
+            sub = a - b;
+
+
+
+
+
+        }
+
+    }
+}
