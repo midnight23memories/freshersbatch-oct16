1451129944b35b1cd5c8b3a84a9fbd7f7fdcdcbd
Darshiikkaa <103111217+Darshiikkaa@users.noreply.github.com>
2024-02-01 17:28:12 +0530

    Add files via upload

---
+++ SampleProj/Program_Darshika.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SampleProj
+{
+    class Program_Darshika
+    {
+        public static void Main(String[] args)
+        {
+            int a;
+            int b;
+            
+            Console.WriteLine("Enter the value of a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+
+            Console.WriteLine("The addition of a and b = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
+    
