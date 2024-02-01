266048e490778d72bf6232d5661a9da8b6219f31
midnight23memories <87180323+midnight23memories@users.noreply.github.com>
2024-02-01 17:15:19 +0530

    Update and rename Program.cs to Program_Anshika.cs

---
+++ SampleProj/Program_Anshika.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int num2 = int.Parse(Console.ReadLine());            
+            float result = num1/num2;
+            Console.WriteLine($"Division of two numbers : {result}");
+            Console.ReadLine();
+        }
+    }
+}
